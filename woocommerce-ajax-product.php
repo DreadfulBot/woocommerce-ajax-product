79236dcf4473b797fc7769238f418f42037c47e0
RiskyWorks <zorg1995@yandex.ru>
2020-11-15 11:59:16 +0300

    scaffold

---
+++ woocommerce-ajax-product.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Woocommerce Ajax Product
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     woocommerce-ajax-product
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Woocommerce_Ajax_Product
+ */
+
+// Your code starts here.
